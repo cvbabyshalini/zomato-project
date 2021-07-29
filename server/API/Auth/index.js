fe43b2b1b470ace5b33c9939dc6d4f1e32cbd3d9
Baby Shalini <chowlababy@gmail.com>
2021-07-29 11:57:25 +0530

    add google authentication

---
+++ server/API/Auth/index.js
@@ -0,0 +1,80 @@
+// Library
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import passport from "passport";
+
+// Models
+import { UserModel } from "../../database/user";
+
+const Router = express.Router();
+
+/*
+Route     /signup
+Des       Register new user
+Params    none
+Access    Public
+Method    POST  
+*/
+Router.post("/signup", async (req, res) => {
+  try {
+    await UserModel.findByEmailAndPhone(req.body.credentials);
+    const newUser = await UserModel.create(req.body.credentials);
+    const token = newUser.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/*
+Route     /signin
+Des       Signin with email and password
+Params    none
+Access    Public
+Method    POST  
+*/
+Router.post("/signin", async (req, res) => {
+  try {
+    const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+
+    const token = user.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/*
+Route     /google
+Des       Google Signin
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
+
+/*
+Route     /google/callback
+Des       Google Signin Callback
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    return res.json({ token: req.session.passport.user.token });
+  }
+);
+
+export default Router;
---
+++ server/index.js
@@ -0,0 +1,43 @@
+// Importing Env Variables
+require("dotenv").config();
+
+// Libraries
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import passport from "passport";
+
+// configs
+import googleAuthConfig from "./config/google.config";
+
+// microservice routes
+import Auth from "./API/Auth";
+
+// Database connection
+import ConnectDB from "./database/connection";
+
+const zomato = express();
+
+// application middlewares
+zomato.use(express.json());
+zomato.use(express.urlencoded({ extended: false }));
+zomato.use(helmet());
+zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+// passport cofiguration
+googleAuthConfig(passport);
+
+// Application Routes
+zomato.use("/auth", Auth);
+
+zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
+
+zomato.listen(4000, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
+);
