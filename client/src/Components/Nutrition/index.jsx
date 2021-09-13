c751cd6c192744e2ee693628ebcf6ed4bc142e3c
Baby Shalini <chowlababy@gmail.com>
2021-09-13 16:16:44 +0530

    add nutrition card

--- client/src/Components/Nutrition/index.jsx
+++ client/src/Components/Nutrition/index.jsx
@@ -3,11 +3,40 @@ import React from 'react'
 //components
 import NutritionCarousel from './NutritionCarousel'
 import NutritionHeroCarousel from './NutritionHeroCarousel'
+import NutritionCard from './NutritionCard'
 const Nutrition = () => {
     return (
         <div>
             <NutritionHeroCarousel/>
-            <NutritionCarousel/>
+            <div className="my-6">
+                <NutritionCarousel/>
+            </div>
+            <div className="flex justify-between flex-wrap lg:px-20 lg:mr-32">
+                <NutritionCard 
+                    bg="red" 
+                    image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+                />
+                <NutritionCard 
+                    bg="red" 
+                    image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+                />
+                <NutritionCard 
+                    bg="red" 
+                    image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+                />
+                <NutritionCard 
+                    bg="red" 
+                    image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+                />
+                <NutritionCard 
+                    bg="red" 
+                    image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+                />
+                <NutritionCard 
+                    bg="red" 
+                    image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+                />
+            </div>
         </div>
     )
 }
