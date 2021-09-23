24421c1413abe8b08bfb81661ba8c72c49bf311b
Baby Shalini <chowlababy@gmail.com>
2021-09-23 20:19:47 +0530

    add restaurant redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import Reviews from "./Pages/Restaurant/Reviews";
 import Menu from "./Pages/Restaurant/Menu";
 import Photos from "./Pages/Restaurant/Photos";
 import Checkout from "./Pages/Checkout";
+import RedirectRestaurant from "./Pages/Restaurant/Redirect";
 
 
 function App() {
@@ -22,9 +23,8 @@ function App() {
     <Route path="/" exact>
         <Redirect to="/delivery" />
     </Route>
-    <Route path="/restaurant/:id" exact>
-        <Redirect to="/restaurant/:id/overview" />
-    </Route>
+    <Route path="/restaurant/:id" exact component={RedirectRestaurant} />
+        
       <HomeLayoutHOC path="/:type" exact component={Home} />
       <RestaurantLayoutHOC 
         path="/restaurant/:id/overview" 
